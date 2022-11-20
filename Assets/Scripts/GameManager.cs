799dcd6747cb57e3ea9d0b6a45db29c30f2ce685
Kacper Suder <ksuder@student.agh.edu.pl>
2022-11-20 07:49:09 +0100

    Przejścia

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject completeLevelUI;
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+}
